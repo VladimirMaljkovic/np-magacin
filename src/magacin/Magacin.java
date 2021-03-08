b9b8efc278f7983440beeb3a91f3dbb78baec242
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-08 23:25:15 +0100

    Napravljena klasa Magacin koja implementira IMagacin

---
+++ src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import interfejs_magacin.IMagacin;
+
+public class Magacin implements IMagacin {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal vratiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
