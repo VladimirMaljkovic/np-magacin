2c87539ee6bc312d97428cadf1a6870e5eaa6509
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-08 23:24:20 +0100

    Popravljena konotrola unosa sa artikal

--- src/magacin/Artikal.java
+++ src/magacin/Artikal.java
@@ -6,28 +6,37 @@ public class Artikal {
 	private int sifra;
 	private String opis;
 	private int kolicina;
+	
 	public String getNaziv() {
 		return naziv;
 	}
 	public void setNaziv(String naziv) {
+		if(naziv == null)
+			throw new RuntimeException("naziv ne sme biti null");
 		this.naziv = naziv;
 	}
 	public int getSifra() {
 		return sifra;
 	}
 	public void setSifra(int sifra) {
+		if(sifra < 0)
+			throw new RuntimeException("sifra ne sme biti manja od 0");
 		this.sifra = sifra;
 	}
 	public String getOpis() {
 		return opis;
 	}
 	public void setOpis(String opis) {
+		if(opis == null)
+			throw new RuntimeException("opis ne sme biti null");
 		this.opis = opis;
 	}
 	public int getKolicina() {
 		return kolicina;
 	}
 	public void setKolicina(int kolicina) {
+		if(kolicina < 0)
+			throw new RuntimeException("kolicina ne sme biti manja od 0");
 		this.kolicina = kolicina;
 	}
 	@Override
