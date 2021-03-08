4240288276535dc673dbfdf990458c44daad42e2
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-08 22:07:15 +0100

    Inicijalni commit. Kreirana klasa Artikal.

---
+++ src/magacin/Artikal.java
@@ -0,0 +1,36 @@
+package magacin;
+
+public class Artikal {
+
+	private String naziv;
+	private int sifra;
+	private String opis;
+	private int kolicina;
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+	public int getKolicina() {
+		return kolicina;
+	}
+	public void setKolicina(int kolicina) {
+		this.kolicina = kolicina;
+	}
+	
+	
+	
+}
